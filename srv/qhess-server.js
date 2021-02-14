9c10dfc4c3e01d7c9c623f3ebdb054b6d6242a15
d-mckee <devon.l.mckee@gmail.com>
2021-02-13 22:25:31 -0800

    Modifying server code and Game class, should be closer to working with client/lib soon

--- srv/qhess-server.js
+++ srv/qhess-server.js
@@ -1,25 +1,40 @@
-//const { v4 : uuid } = require('uuid');
+const { v4 : uuid } = require('uuid');
 const { Board } = require('../lib/qhess.js');
 const io = require('socket.io');
 
 class Game {
     constructor(player) {
+        this.id = uuid();
+        this.ip = player.address;
         this.turn = player.id;
         this.players = [player.id];
         this.board = new Board();
     }
+
     addPlayer(player) {
         if (players.length < 2) {
             this.players.push(player.id);
         }
     }
-    movePiece() {
-        
+
+    movePiece(id, to, e) {
+        let piece = this.board.findPiece(id);
+        let x, y = to;
+        piece.moveTo(x, y, e);
+        this.changeTurn();
     }
+    
     changeTurn() {
-        
+        if (this.turn == this.players[0]) {
+            this.turn = this.players[1];
+        } else {
+            this.turn = this.players[0];
+        }
     }
 
+    updateBoard() {
+        this.players.forEach(p => io.sockets.socket(p).emit('board_update', this.board.board));
+    }
 }
 
 module.exports = { Game };
--- srv/server.js
+++ srv/server.js
@@ -1,11 +1,19 @@
 // Constants
 const BROWSER_PORT = 8080;
-const SOCKET_PORT = 8081;
+const SOCKET_PORT = 8080;
 
 // Requires
 const path = require('path');
 const express = require('express');
 const io = require('socket.io') (SOCKET_PORT);
+/* BELOW WILL BE REQUIRED WHEN WE ARE NO LONGER USING LOCALHOST BECAUSE OF CORS (https://socket.io/docs/v3/handling-cors/)
+const io = require('socket.io') (httpServer, {
+    cors: {
+        origin: 'http://localhost',
+        methods: ["GET", "POST"]
+    }
+})
+*/
 const { Game, Player } = require('./qhess-server.js');
 
 // Containers
@@ -25,26 +33,43 @@ console.log(`Server started on port ${BROWSER_PORT}`);
 // Listening for new socket connections
 console.log(`Listening for new socket connections on ${SOCKET_PORT}`);
 io.on('connection', socket => {
-    socket.send();
-
     socket.on('connect', (data) => {
-        console.log(data);
+        if(data) {
+            console.log(`Client ${socket.id} (${socket.address}) connected with data: ${data}`);
+        } else {
+            console.log(`Client ${socket.id} (${socket.address}) connected`);
+        }
     });
 
+    // Debugging
     socket.on('message', (data) => {
         console.log(data);
     });
 
     // Game events
     socket.on('create_game', (data) => {
-        console.log(data);
+        if(!games.some(g => g.ip == socket.address)) {
+            games.push(new Game(socket));
+        } else {
+            socket.emit("error", "Game still active for IP");
+        }
     });
 
-    socket.on('join_game', (data) => {
-        console.log(data);
+    socket.on('join_game', (gid) => {
+        if(games.some(g => g.id == gid)) {
+            let game = games.find(g => g.id == gid);
+            game.addPlayer(socket); 
+        } else {
+            socket.emit("error", "Game not found");
+        }
     });
 
     socket.on('move_piece', (data) => {
+        
+    });
+    // requires something like { id: <UUID>, to: [2, 4], energy: 4 }
+
+    socket.on('leave_game', (data) => {
         console.log(data);
     });
 });
